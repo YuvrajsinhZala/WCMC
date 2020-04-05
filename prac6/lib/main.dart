123aa22b5cc068ad53624836c34960eb6a7cf152
Yuvrajsinh Zala <17it131@charusat.edu.in>
2020-04-05 17:07:37 +0530

    6th Practical

---
+++ prac6/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:torch/torch.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '17it131_Practical-6',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('17it131_Practical-6'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  RaisedButton(
+                      child: Text(
+                        'Flash On',
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      color: Colors.teal[300],
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Torch.turnOn();
+                      }),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  RaisedButton(
+                      child: Text(
+                        'Flash Off',
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      color: Colors.teal[300],
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Torch.turnOff();
+                      }),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
