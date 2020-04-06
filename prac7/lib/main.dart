34ecf592ef64a187537dac4e8c92889601c5cfc0
Yuvrajsinh Zala <17it131@charusat.edu.in>
2020-04-06 12:44:20 +0530

    7th Practical

---
+++ prac7/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _PopoupState createState() => _PopoupState();
+}
+
+class _PopoupState extends State<MyApp> {
+  Color bgColor;
+  void choiceAction(String choice) {
+    if (choice == Constants.White) {
+      setState(() {
+        bgColor = Colors.white;
+      });
+    }
+    if (choice == Constants.Black) {
+      setState(() {
+        bgColor = Colors.black;
+      });
+    }
+    if (choice == Constants.Red) {
+      setState(() {
+        bgColor = Colors.red;
+      });
+    }
+    if (choice == Constants.Blue) {
+      setState(() {
+        bgColor = Colors.blue;
+      });
+    }
+    if (choice == Constants.Green) {
+      setState(() {
+        bgColor = Colors.green;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '17it131_Practical-7',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('17it131_Practical-7'),
+          backgroundColor: Colors.deepPurpleAccent,
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context) {
+                return Constants.choices.map((String choice) {
+                  return PopupMenuItem<String>(
+                      value: choice, child: Text(choice));
+                }).toList();
+              },
+            )
+          ],
+        ),
+        backgroundColor: bgColor,
+      ),
+    );
+  }
+}
+
+class Constants {
+  static const String White = 'White';
+  static const String Black = 'Black';
+  static const String Red = 'Red';
+  static const String Blue = 'Blue';
+  static const String Green = 'Green';
+  static const List<String> choices = <String>[White, Black, Red, Blue, Green];
+}
