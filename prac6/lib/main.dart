141aef40536d4b2116cdb956a5f20fe7ab1a68e4
Yuvrajsinh Zala <17it131@charusat.edu.in>
2020-04-06 12:56:51 +0530

    6th Practical

--- prac6/lib/main.dart
+++ prac6/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '17it131_Practical-6',
+      title: '17it131_Practical6',
       home: Scaffold(
         appBar: AppBar(
           title: Text('17it131_Practical-6'),
