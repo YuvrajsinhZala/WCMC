9094f693d1b0c366cfc792662affdc64313fbcd2
Yuvrajsinh Zala <17it131@charusat.edu.in>
2020-04-06 12:30:51 +0530

    2nd Practical

--- prac2/test/widget_test.dart
+++ prac2/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prac2/main.dart';
+import 'package:practical_2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
